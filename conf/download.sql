5d9105acff3b19e3d25a23f3cb686eb4b08767ed
Sandro Neumann <sandro.neumann@idgis.nl>
2016-05-27 17:14:39 +0200

    Changed sql file to retrieve values from jsob b column

--- conf/download.sql
+++ conf/download.sql
@@ -1,4 +1,7 @@
-select req.request_id, req.request_time, req.user_name, req.user_emailaddress, req.user_format, res.response_time, res.response_code
-	from request_info req 
-	join result_info res on req.id = res.request_info_id
+select req.request_id, req.request_time, req.download -> 'ft' ->> 'crs' as crs, req.download -> 'ft' ->> 'name' as title, 
+	req.download -> 'ft' ->> 'serviceUrl' as serice_url, req.download -> 'ft' ->> 'wfsMimetype' as wfs_mimetype, 
+	req.download -> 'ft' ->> 'serviceVersion' as service_version, req.download ->> 'name' as uuid, req.user_name, req.user_emailaddress, 
+	req.user_format, res.response_time, res.response_code
+		from request_info req 
+		join result_info res on req.id = res.request_info_id
 ;
